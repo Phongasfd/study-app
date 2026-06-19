2b7d5e68b10d0eaaacf688b1a87a93f3d6a28001
Nam Phong <nguyennamphong210@gmail.com>
2026-06-19 10:20:37 +0700

    disable springboot test, and sync package.json and package-lock.json

--- backend/src/test/java/com/namphong/backend/BackendApplicationTests.java
+++ backend/src/test/java/com/namphong/backend/BackendApplicationTests.java
@@ -1,8 +1,11 @@
 package com.namphong.backend;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+
+@Disabled
 @SpringBootTest
 class BackendApplicationTests {
 
